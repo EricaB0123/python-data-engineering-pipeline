6d5f31f5d282123ee49a12ecb60a5ecae52689b7
EricaB0123 <erica.li.bradley@gmail.com>
2026-02-23 18:57:13 +1300

    folder setup

---
+++ src/utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        file_handler = logging.FileHandler(os.path.join(LOG_DIR, f"{name}.log"))
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
